4dc6a06e057d8633e23f0e9aa743079928815e1e
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-08 00:00:48 -0800

    Spacing Only

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-  $('#light').click(function(fun){
-	console.log("hey");
-	jQuery('.groupl').css('color: blue;')
-	});
+
+    $('#light').click(function(fun){
+        console.log("hey");
+        jQuery('.groupl').css('color: blue;')
+    });
 });
 
 $(document).ready(function() {
+
     $('#dark').click(function(fun){
-	console.log("ho");
-});
+        console.log("ho");
+    });
 });
