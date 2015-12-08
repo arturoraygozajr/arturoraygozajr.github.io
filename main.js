7c9130f95dad244701ddc3e649e6b17d196ad2b9
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-08 00:56:48 -0800

    Dark theme link functional removed purple from css added "#" to links to prevent reload

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 
+    jQuery('.print').css("color", "purple");
+
     $('#light').click(function(fun){
         console.log("hey");
-        jQuery('.groupl').css('color', 'blue')
+        jQuery('.groupl').css('color', 'blue');
     });
 
     $('#dark').click(function(fun){
         console.log("ho");
     });
+
+    $('#makedark').click(function(fun){
+        console.log("dark");
+        jQuery('body').css('color','white');
+        jQuery('body').css('background-color','black');
+
+        // Also we need to remove the purple on the first text because it will
+        // overiride the white
+        //NOTE: this only works if the color was applied by jquery
+        jQuery('.print').css("color", "");
+
+    });
+
+
 });
