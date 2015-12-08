7f126c0d33b496b15b39a961d8c8832653041573
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-08 01:28:04 -0800

    Noted out buttons Changed text Removed purple text Change load to match dark setting Change links font size

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-    jQuery('.print').css("color", "purple");
 
     $('#light').click(function(fun){
         console.log("hey");
@@ -11,15 +10,20 @@ $(document).ready(function() {
         console.log("ho");
     });
 
-    $('#makedark').click(function(fun){
+    $('#makedark').click(function(fun) {
         console.log("dark");
-        jQuery('body').css('color','white');
-        jQuery('body').css('background-color','black');
+        jQuery('body').css('color', 'white');
+        jQuery('body').css('background-color', 'black');
+    });
 
         // Also we need to remove the purple on the first text because it will
         // overiride the white
         //NOTE: this only works if the color was applied by jquery
-        jQuery('.print').css("color", "");
+        //jQuery('.print').css("color", "");
+        $('#makelight').click(function(fun){
+            console.log("light");
+            jQuery('body').css('color','black');
+            jQuery('body').css('background-color','white');
 
     });
 
