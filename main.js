2269729c2353e1ac4f4e83b853251f20bb60f1ed
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-08 00:27:37 -0800

    fixed jQuery .css() combined both .ready created links

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
 
     $('#light').click(function(fun){
         console.log("hey");
-        jQuery('.groupl').css('color: blue;')
+        jQuery('.groupl').css('color', 'blue')
     });
-});
-
-$(document).ready(function() {
 
     $('#dark').click(function(fun){
         console.log("ho");
