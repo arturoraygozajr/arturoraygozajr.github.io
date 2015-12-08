2a00bc5bd5a078a5ec2e667d913a78e88bb11a7e
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-07 22:27:16 -0800

    console logs hey and ho

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#light').click(function(fun){
+	console.log("hey");
+});
+});
+
+$(document).ready(function() {
+    $('#dark').click(function(fun){
+	console.log("ho");
+});
+});
