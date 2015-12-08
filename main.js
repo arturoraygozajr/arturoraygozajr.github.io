1ec9aa47b877559e6fc72b736174f655cf439e5b
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-07 23:58:43 -0800

    css color blue

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-    $('#light').click(function(fun){
+  $('#light').click(function(fun){
 	console.log("hey");
-});
+	jQuery('.groupl').css('color: blue;')
+	});
 });
 
 $(document).ready(function() {
