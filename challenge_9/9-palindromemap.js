5433aa22b33ff36df1b4fe72fdb62715d23d38af
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-20 15:24:28 -0800

    math functions

---
+++ challenge_9/9-palindromemap.js
@@ -0,0 +1,14 @@
+var add = function(Content1, Content2){
+    var result = (Content1+Content2);
+    return result;
+};
+
+console.log(add(13,16));
+
+var subtract = function(Content1, Content2){
+    var result = (Content1 - Content2);
+    return result;
+};
+
+
+console.log(subtract(213,134));
