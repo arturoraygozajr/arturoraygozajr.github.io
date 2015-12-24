ee44dff9fcc6d60d2a7b90be5bfd0042df3355c6
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-23 21:31:48 -0800

    arrayAnalyzer built. need to be cleaned up.

--- challenge_11/challenge_11.js
+++ challenge_11/challenge_11.js
@@ -1,7 +1,31 @@
-var elementLoop = function(arr){
+function arrayAnalyzer(arr){
     for (var i = 0; i < arr.length; i++){
-        console.log(arr[i]);
+        this.odds = this.odds || 0;
+        if (arr[i]%2 != 0){
+            this.odds++
+        };
+
+        this.negatives = this.negatives || 0;
+        if(arr[i] < 0){
+          this.negatives++
+        };
+
+        var total = total + arr[i] || arr[i];
+        };
+        this.avg = parseFloat(parseFloat(total/ arr.length).toFixed(2));
+
+    var medianFind = function(values){
+        values.sort( function(a,b) {return a - b;} );
+
+        var half = Math.floor(values.length/2);
+
+        if(values.length % 2)
+            return values[half];
+        else
+            return (values[half-1] + values[half]) / 2.0;
     };
+    this.median = medianFind(arr);
 };
 
-elementLoop(["wassup", "jammy", "whaa"])
+var p = new arrayAnalyzer([7, -3, 0, 12, 44, -5, 3]);
+console.log(p)
