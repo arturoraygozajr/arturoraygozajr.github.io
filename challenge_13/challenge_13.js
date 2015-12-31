56ed0add7c5da8187480ad10e51cb685b876730c
Arturo Raygoza <arturoraygozajr@gmail.com>
2015-12-30 20:23:09 -0800

    Completed challenge_13

--- challenge_13/challenge_13.js
+++ challenge_13/challenge_13.js
@@ -1,11 +1,49 @@
 
+function invertColor(hexTripletColor) {
+    var color = hexTripletColor;
+    color = color.substring(1);           // remove #
+    color = parseInt(color, 16);          // convert to integer
+    color = 0xFFFFFF ^ color;             // invert three bytes
+    color = color.toString(16);           // convert to hex
+    color = ("000000" + color).slice(-6); // pad with leading zeros
+    color = "#" + color;                  // prepend #
+    return color;
+}
+
+
+function rgbToHex(srgb) {
+    var rgbvals = /rgba?\((.+),(.+),(.+)\)/i.exec(srgb);
+    var rval = parseInt(rgbvals[1]);
+    var gval = parseInt(rgbvals[2]);
+    var bval = parseInt(rgbvals[3]);
+    return '#' + (
+        rval.toString(16) +
+            gval.toString(16) +
+            bval.toString(16)
+        ).toUpperCase();
+}
 
 
 function loop_div()
 {
+    // pick random colors
     $( "div" ).each(function( index ) {
-        console.log( index + ": " + $( this ).text() );
+        var rand = '#'+Math.floor(Math.random()*16777215).toString(16);
+        $( this).css('background-color',rand);
     });
+
+
+    // Invert background colors of all divs
+    $( "div" ).each(function( index ) {
+        var x = $(this).css('background-color');
+        console.log("x "+ x);
+        hexcolor = rgbToHex(x);
+       $( this).css('background-color',invertColor(hexcolor));
+    });
+
+
+
+
 }
 
 
